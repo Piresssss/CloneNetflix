9a3aa1c4509c8b5bbf5375c4fef6ec8a2364dccd
Piresssss <faelpiressilva@hotmail.com>
2021-12-16 20:32:02 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(()=>{
     const loadAll = async () => {
@@ -25,8 +27,24 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
+
+      <Header black={blackHeader}/>
 
       {featuredData &&
         <FeaturedMovie item={featuredData} />
@@ -37,6 +55,18 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito por Raphael Pires<br/>
+        Direitos de imagem para Netflix<br/>
+        API disponibilizada pelo site Themoviedb.org<br/>
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"/>
+        </div>
+      }
     </div>
   )
 };
